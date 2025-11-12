898b732c47c0334196e7df0c0683ce63e645fd01
ayeshatahreem2006 <ayeshatahreem1983@gmail.com>
2025-11-13 00:17:09 +0530

    Deleted ptr8.c

--- ptr8.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(int argCount, char *args[]){
-    float num=5.5f;
-    printf("value of Num = %2.f \n", num);
-    printf("Address of Num = %p \n", &num);
-    printf("Value of Num = %1.f \n", *(&num));
-}
